1632da32703a4c995b6767f6fe9e5e20b6396cac
Danielpalma54 <danielpalma212002@gmail.com>
2026-04-15 13:24:30 -0500

    Import tipo de letra

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: '',
-  token: 'MDRmZGY4NzM3MzU5M2M1YzkwNzVkYTMxMGE5YTIxN2QzNzU0YTAwZjI1NWY1MTdkNDczZmVjMTE1YzNkYTg4Yg==',
+  token: 'MDRmZGY4NzM3MzU5M2M1YzkwNzVkYTMxMGE5YTIxN2QzNzU0YTAwZjI1NWY1MTdkNDczZmVjMTE1YzNkYTg4Yg==', //token de prueba? 
   loginUrl: 'https://www.facturasintuitosa.com/',
   portalUrl: 'https://www.facturasintuitosa.com/'
 };
